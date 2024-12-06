88c3f83de6af9c4c88181e9461bf8b5e2197de47
bear <xxrBear@gmail.com>
2024-12-06 13:24:05 +0800

    ✨ 添加 检查用户管理员权限中间件

--- common/middleware.py
+++ common/middleware.py
@@ -1,9 +1,11 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 
+from common import state
 from common.resp import json_data
 
 OPEN_URL = ['/docs', '/openapi.json', '/redoc']
 SKIP_URL = ['/api/user/register', '/api/user/login', '/', '/api/user/get/login']
+ADMIN_URL = []
 
 
 class UserLoginStateMiddleware(BaseHTTPMiddleware):
@@ -18,6 +20,24 @@ class UserLoginStateMiddleware(BaseHTTPMiddleware):
         if request_path not in OPEN_URL + SKIP_URL:
             user_login_state = request.session.get('user_login_state')
             if not user_login_state:
-                return json_data(**{'code': 40010, 'message': '用户未登录'})
+                return json_data(**{'code': state.USER_NOT_LOGIN, 'message': '用户未登录'})
+        response = await call_next(request)
+        return response
+
+
+class CheckAdminMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        """
+        中间件统一检查管理员请求
+        :param request:
+        :param call_next:
+        :return:
+        """
+        request_path = request.url.path
+        if request_path in ADMIN_URL:
+            user_login_state = request.session.get('user_login_state')
+            user_role = user_login_state.get('user_role')
+            if user_role != 'admin':
+                return json_data(**{'code': state.NO_AUTH_ERROR, 'message': '用户无权限'})
         response = await call_next(request)
         return response
